8f84980be7dafed0cda0bd9fa61f5951f49ff0e7
Rafaela Pinter <rpinter811@gmail.com>
2024-03-27 21:12:00 -0400

    new beziers

--- bezier.js
+++ bezier.js
@@ -3,6 +3,7 @@
 References:
 https://www.youtube.com/watch?v=uctX1P3H3xM
 https://editor.p5js.org/PeterQwertz/sketches/Mh7aSsyMl
+// https://github.com/rethread-studio/algorithmic-art-course/blob/main/examples/template-p5-to-svg.html
 
 
 vec_of_bezier_lines = [...,...,...]
@@ -10,77 +11,148 @@ new_point = point
 vec_of_bezier_lines[i % len(vec)] = new_point
 */
 
-let pause = false
+var w
+var h
+var rightmargin
+var leftmargin
+var topmargin
+var bottommargin
+var actualwidth
+var actualheight
+
+// playground
 let x1, y1, x2, y2, x3, y3, x4, y4
-let i = 30
-let offset = 100
-let speed = 0.005
+let i = 100
+let offset = 2
+let speed
 let j1 = -100
 let j2 = -100
 let j3 = 100
 let j4 = 100
 
+let steps = 200
 
 function setup() {
-    createCanvas(windowWidth, windowHeight)
-    background(5)
-    frameRate(15)
+
+    w = windowWidth
+    h = windowHeight
+    rightmargin = 0.9 * w
+    leftmargin = 0.1 * w
+    topmargin = 0.1 * h
+    bottommargin = 0.9 * h
+    actualwidth = rightmargin - leftmargin
+    actualheight = bottommargin - topmargin
+    createCanvas(w, h)
+    clear()
+    // noLoop()
+    frameRate(0.5)
 }
 
 function draw() {
-    // background(0)
-
-    // noLoop()
-
     bezier_func(1)
-
-    // Tests:
-    // call_bezier(2)
+    draw_text()
 }
-
 
 function bezier_func(l) {
-    // background(5, 5, 5, 6)
     noFill()
-    strokeWeight(1)
+    strokeWeight(1.5)
+    background(0)
+    let choice = random([0, 1, 2])
+    let r1 = random(0, 255)
+    let r2 = random(0, 255)
+    let g1 = random(0, 255)
+    let g2 = random(0, 255)
+    let b1 = random(0, 255)
+    let b2 = random(0, 255)
+    speed = random(0.003, 0.006)
+    if (choice != 2) {
+        for (let j = 0; j < steps; j++) {
 
-    stroke(
-        200, 0, 0, // red
-        // 200, 200, 0, // yellow
-        // noise(offset + i) * 300,
-        // noise(offset + i * 2) * 300, // adds color
-        // noise(offset + i * 3) * 300, // adds color
-        40
-    )
+            x1 = (noise(offset + i) * width + j1) * l
+            y1 = (noise(offset + i + 2) * height + j1) * l
+            x2 = (noise(offset + i + 3) * width + j2) * l
+            y2 = (noise(offset + i + 4) * height + j2) * l
+            x3 = (noise(offset + i + 5) * width + j3) * l
+            y3 = (noise(offset + i + 6) * height + j3) * l
+            x4 = (noise(offset + i + 7) * width + j4) * l
+            y4 = (noise(offset + i + 8) * height + j4) * l
 
-    x1 = (noise(offset + i) * width + j1) * l
-    y1 = (noise(offset + i * 2) * height + j1) * l
-    x2 = (noise(offset + i * 3) * width + j2) * l
-    y2 = (noise(offset + i * 4) * height + j2) * l
-    x3 = (noise(offset + i * 5) * width + j3) * l
-    y3 = (noise(offset + i * 6) * height + j3) * l
-    x4 = (noise(offset + i * 7) * width + j4) * l
-    y4 = (noise(offset + i * 8) * height + j4) * l
+            if (choice == 0) {
+                stroke(r1, b1, g1)
+                bezier(x1, y1, x2, y2, x3, y3, x4, y4)
+                line(leftmargin, x1, x1, y1)
+                let max_right
+                if (y4 > rightmargin) {
+                    max_right = rightmargin
+                } else {
+                    max_right = y4
+                }
+                line(x4, y4, max_right, bottommargin)
+            }
+            else if (choice == 1) {
+                stroke(r1, g1, b1)
+                bezier(x1, y1, x2, y2, x3, y3, x4, y4)
+                stroke(r2, g2, b2)
+                line(x3, y3, rightmargin, y3)
+                line(x2, bottommargin, x2, y2)
+            }
 
-    bezier(
-        x1, y1,
-        x2, y2,
-        x3, y3,
-        x4, y4,
-    )
-    offset += speed
+            offset += speed
+        }
+    } else {
+        for (let j = 0; j < steps; j++) {
+            stroke(r1, g1, b1)
+            x1 = (noise(offset + i) * width + j1) * l
+            y1 = (noise(offset + i * 52) * height + j1) * l
+            x2 = (noise(offset + i * 53) * width + j2) * l - 100
+            y2 = (noise(offset + i * 54) * height + j2) * l - 100
+            x3 = (noise(offset + i * 55) * width + j3) * l + 100
+            y3 = (noise(offset + i * 56) * height + j3) * l + 100
+            x4 = (noise(offset + i * 57) * width + j4) * l
+            y4 = (noise(offset + i * 58) * height + j4) * l
+
+            bezier(x1, y1, x2, y2, x3, y3, x4, y4)
+
+            offset += speed
+        }
+        for (let j = 0; j < steps; j++) {
+            stroke(r2, g2, b2)
+            // stroke(0)
+            x1 = (noise(offset + i) * width + j1) * l - 150
+            y1 = (noise(offset + i * 2) * height + j1) * l + 150
+            x2 = (noise(offset + i * 3) * width + j2) * l
+            y2 = (noise(offset + i * 4) * height + j2) * l
+            x3 = (noise(offset + i * 5) * width + j3) * l
+            y3 = (noise(offset + i * 6) * height + j3) * l
+            x4 = (noise(offset + i * 7) * width + j4) * l + 150
+            y4 = (noise(offset + i * 8) * height + j4) * l + 150
+
+            bezier(x1, y1, x2, y2, x3, y3, x4, y4)
+            offset += speed
+        }
+    }
 }
 
-function mousePressed() {
-    setup()
-}
+function draw_text() {
+    stroke(random(0, 255))
+    textSize(12)
+    textFont("Courier New")
+    text(`
+    for (let j = 0; j < steps; j++) {
 
-function call_bezier(l) {
-    if (l < 0.1) {
-        return
+        stroke(r1, g1, b1)
+        x1 = (noise(offset + i) * width + j1) * l
+        y1 = (noise(offset + i + 2) * height + j1) * l
+        x2 = (noise(offset + i + 3) * width + j2) * l
+        y2 = (noise(offset + i + 4) * height + j2) * l
+        x3 = (noise(offset + i + 5) * width + j3) * l
+        y3 = (noise(offset + i + 6) * height + j3) * l
+        x4 = (noise(offset + i + 7) * width + j4) * l
+        y4 = (noise(offset + i + 8) * height + j4) * l
+
+        bezier(x1, y1, x2, y2, x3, y3, x4, y4)
+
+        offset += speed
     }
-    else {
-        bezier_func(l)
-        call_bezier(l / 2)
-    }
+    `, random(100, windowWidth - 100), random(100, windowHeight - 150), 500)
 }
